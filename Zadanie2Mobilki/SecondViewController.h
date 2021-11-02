0ad20aa8b571a284e5ed20f5e2fb28b64627297c
ArralS <80539257+ArralS@users.noreply.github.com>
2021-11-02 16:07:34 +0100

    first commit

---
+++ Zadanie2Mobilki/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Zadanie2Mobilki
+//
+//  Created by Reynard on 11/2/21.
+//  Copyright © 2021 Reynard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
